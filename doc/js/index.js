412d259691288b1ad80491784c29f3bbade490ec
Siva Sivakumar <siva.sivakumar@nokia.com>
2023-02-19 15:37:35 +0100

    update

--- doc/js/index.js
+++ doc/js/index.js
@@ -1,18 +1,26 @@
 // MENU TOGGLE
+/*function toggleMenu() {
+  const menu = document.getElementById("sideMenu");
+  const mbtn = document.getElementById("menuButton");
+  const mcn = menu.className;
+  const animation = menu.style.animationName;
+  if(animation == "" || animation == "fadeOutLeft") {
+    menu.style.animationName = "fadeInLeft";
+    mbtn.innerHTML = "<i class='bx bx-x bx-sm'></i>";
+  } else {
+    menu.style.animationName = "fadeOutLeft";
+    mbtn.innerHTML = "<i class='bx bx-menu-alt-left bx-sm'></i>";
+  }
+}*/
+
 function toggleMenu() {
   const menu = document.getElementById("sideMenu");
   const mcn = menu.className;
-  const hcn = "is-hidden-touch";
-  const fadeIn = "animate__fadeInLeft";
-  const fadeOut = "animate__fadeOutLeft";
-  if(mcn.includes(fadeIn)) {
-    menu.classList.remove(hcn);
-    menu.classList.remove(fadeIn);
-    menu.classList.add(fadeOut);
-  } 
-  else if(mcn.includes(fadeOut)) {
-    menu.classList.remove(fadeOut)
-    menu.classList.add(fadeIn);
+  const hideClass = "is-hidden-touch";
+  if(mcn.includes(hideClass)) {
+    menu.classList.remove(hideClass)
+  } else {
+    menu.classList.add(hideClass)
   }
 }
 
@@ -21,19 +29,6 @@ const service = document.getElementsByClassName("service");
 const menuItem = document.getElementsByClassName("list-item");
 Array.from(service).forEach(el => el.addEventListener("click", toggleMenu));
 Array.from(menuItem).forEach(el => el.addEventListener("click", toggleMenu));
-
-/*
-// NAVBAR TOGGLE
-const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"), 0);
-$navbarBurgers.forEach( el => {
-  el.addEventListener("click", () => {
-    const target = el.dataset.target;
-    const $target = document.getElementById(target);
-    el.classList.toggle("is-active");
-    $target.classList.toggle("is-active");
-  });
-});
-*/
 
 function searchSideMenu() {
   const searchKey = document.getElementById("search");
